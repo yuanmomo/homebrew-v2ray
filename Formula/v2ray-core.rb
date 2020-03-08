bbc8ea9f7f4c2c32090ace106b119d0399408894
aaaaa <aaaa@gmail.com>
2020-03-08 14:57:41 +0800

    update v2ray to v4.21.3

--- Formula/v2ray-core.rb
+++ Formula/v2ray-core.rb
@@ -5,9 +5,9 @@
 class V2rayCore < Formula
   desc "A platform for building proxies to bypass network restrictions."
   homepage "https://www.v2ray.com/"
-  url "https://github.com/v2ray/v2ray-core/releases/download/v4.22.1/v2ray-macos.zip"
-  version "4.22.1"
-  sha256 "dc743916843e0b2035274e6ac8ac9f1f47ebdb42fee08ef2c5cc277d08d3afda"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v4.21.3/v2ray-macos.zip"
+  version "4.21.3"
+  sha256 "924c4348812b6a270344dabff17eaf10e2062ce5dd9b2eee095fe1f0491a29e0"
 
 
 
